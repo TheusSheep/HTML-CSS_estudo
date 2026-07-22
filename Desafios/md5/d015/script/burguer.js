1d4d6e98d32696d50234443daaee49c5b7c89a74
Matheus Guilherme Brito <mggodmine@gmail.com>
2026-07-22 11:28:41 -0300

    Módulo 5 - d015 v3
    Finalmente um meno de hamburguer funcional

--- Desafios/md5/d015/script/burguer.js
+++ Desafios/md5/d015/script/burguer.js
@@ -1,6 +1,17 @@
-const menuclick = document.getElementById('burguer')
-const menulink = document.getElementById('option')
+const menuabrir = document.getElementById('menu')
+const menufecha = document.getElementById('menux')
 
-menuclick.addEventListener("click", function(){
-    menulink.style.display = 'flex'
+menuabrir.addEventListener("click", function(){
+    document.getElementById('textmenu').style.display = 'none'
+    document.getElementById('option').style.display = 'flex'
+    document.getElementById('menu').style.display = 'none'
+    document.getElementById('menux').style.display = 'block'
+    document.querySelector('header').style.flexFlow = 'column, nowrap'
+})
+
+menufecha.addEventListener("click", function(){
+    document.getElementById('textmenu').style.display = ''
+    document.getElementById('option').style.display = ''
+    document.getElementById('menu').style.display = 'block'
+    document.getElementById('menux').style.display = 'none'
 })
